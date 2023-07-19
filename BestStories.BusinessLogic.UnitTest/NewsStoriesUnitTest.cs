f676684200eea2c4a916daadd0def2a9218b10bc
amdas <amdas@teksystems.com>
2023-07-19 17:03:44 +0100

    update Unit test cases

--- BestStories.BusinessLogic.UnitTest/NewsStoriesUnitTest.cs
+++ BestStories.BusinessLogic.UnitTest/NewsStoriesUnitTest.cs
@@ -15,12 +15,12 @@ namespace BestStories.BusinessLogic.UnitTest
         public async Task ShouldReturnIsValidResponseFromGetNewsStroriesAsync()
         {
             var repoMock = new Mock<IHackerNewsRepository>();
-            List<int> ids = new List<int> { 1, 2, 3 };
+            List<int> ids = new List<int> { 36757542, 36774627, 36753225 };
             List<Entity> list = new List<Entity> { new Entity { Id= 36757542, Title = "Llama 2", Url = "https://ai.meta.com/llama/", By = "friggeri", Score = 2189 } };
             repoMock.Setup(p => p.GetBestStoriesAsync()).ReturnsAsync(ids);
-            repoMock.Setup(c => c.GetStoryAsync(1)).ReturnsAsync(new Entity { Id = 36757542, Title = "Llama 2", Url = "https://ai.meta.com/llama/", By = "friggeri", Score = 2189 });
-            repoMock.Setup(c => c.GetStoryAsync(2)).ReturnsAsync(new Entity { Id = 36774627, Title = "Llama 3", Url = "https://ai.meta.com/llama/", By = "friggeri", Score = 2190 });
-            repoMock.Setup(c => c.GetStoryAsync(3)).ReturnsAsync(new Entity { Id = 36753225, Title = "Llama 4", Url = "https://ai.meta.com/llama/", By = "friggeri", Score = 2178 });
+            repoMock.Setup(c => c.GetStoryAsync(36757542)).ReturnsAsync(new Entity { Id = 36757542, Title = "Llama 2", Url = "https://ai.meta.com/llama/", By = "friggeri", Score = 2189 });
+            repoMock.Setup(c => c.GetStoryAsync(36774627)).ReturnsAsync(new Entity { Id = 36774627, Title = "Llama 3", Url = "https://ai.meta.com/llama/", By = "friggeri", Score = 2190 });
+            repoMock.Setup(c => c.GetStoryAsync(36753225)).ReturnsAsync(new Entity { Id = 36753225, Title = "Llama 4", Url = "https://ai.meta.com/llama/", By = "friggeri", Score = 2178 });
             var sut = new NewsStories(repoMock.Object);
             var result = await sut.GetNewsStroriesAsync(1);
             result.Should().BeEquivalentTo(list);
